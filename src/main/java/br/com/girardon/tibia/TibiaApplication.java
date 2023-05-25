0c6a30f4bede32f0d00aba26b7c81c5a4d129495
Guto Girardon <ggirardon@gmail.com>
2023-05-24 23:24:44 -0300

    feature: Create initial spring project

---
+++ src/main/java/br/com/girardon/tibia/TibiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.girardon.tibia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TibiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TibiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/girardon/tibia/TibiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.girardon.tibia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TibiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
